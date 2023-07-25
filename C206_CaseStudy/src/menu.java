13c95c0f175e52558821c4d906cc0ef4da8e30d7
lucas <lucas@kuroshihebi>
2023-07-25 15:58:24 +0800

    Added the Helper class Added the menu class Added the order class Added the payment class Added the School class Added the user class Added the vendor class
    Each person do one class refer to the WS for which class is to be done

---
+++ C206_CaseStudy/src/menu.java
@@ -0,0 +1,18 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name:lucas
+ * Student ID: {22017175}
+ * Class: {W65E}
+ * Date/Time created: Tuesday 25-07-2023 15:11
+ */
+
+/**
+ * @author lucas
+ *
+ */
+public class menu {
+
+}
